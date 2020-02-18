c4eb249bb791b23530efc194f6938d73bcaa5811
whgkrud1090 <gkrud1023@gmail.com>
2020-02-18 10:25:44 +0900

    git연결 테스트

---
+++ demo/src/main/java/com/example/demo/DemoApiController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApiController {
+	@GetMapping("/demoapistring")
+	public String demoapistring() {
+		return "가나다라마바사";
+	}
+
+	@GetMapping("/demoapi")
+	public Map<String, Object> demoapi() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "홍길동");
+		map.put("birthday", 15920505);
+		return map;
+	}
+}
